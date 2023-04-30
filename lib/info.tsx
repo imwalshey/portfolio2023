07481b2974a02c4cc67634f27ef9a49ae9cde5c8
imwalshey <imwalshey@proton.me>
2023-04-30 05:10:36 -0400

    updating HOME page

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,22 +1,18 @@
-import me from '../app/avatar.jpg';
+import me from '../app/placeholder.jpg';
 
-export const name = 'Lee Robinson';
+export const name = 'William Walsh';
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Lee. I'm the <b>VP of Developer Experience at Vercel</b> where my
-      team helps developers build a faster web.
+      Hey, I'm a Self-Taught Full-Stack Python & React.js Developer who loves learning new exciting things. 
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I'm a Next.js contributor and help lead our open-source communities. I'm
-      passionate about frontend development and have created courses on React,
-      Next.js, and web development. I'm an advisor and investor in early stage
-      startups.
+      When I'm not coding, I'm probably playing video games or working elsewhere 
     </>
   );
 };
