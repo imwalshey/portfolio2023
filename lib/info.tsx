02c0b87772b030bcfb610de1815f371e7da38a2e
imwalshey <imwalshey@proton.me>
2023-04-30 05:28:26 -0400

    still trying to update

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,18 +1,19 @@
-import me from '../app/placeholder.jpg';
+import me from "../app/placeholder.jpg";
 
-export const name = 'William Walsh';
+export const name = "William Walsh";
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm a Self-Taught Full-Stack Python & React.js Developer who loves learning new exciting things. 
+      Hey, I'm a Self-Taught Full-Stack Python & React.js Developer who loves
+      learning new exciting things.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing video games or working elsewhere 
+      When I'm not coding, I'm probably playing video games or working elsewhere
     </>
   );
 };
